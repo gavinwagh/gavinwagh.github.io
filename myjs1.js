d9229cc447c1cc81cb1abc38c118a2ffd270ed96
gavinwagh <56243312+gavinwagh@users.noreply.github.com>
2019-10-13 22:24:01 +0530

    Add files via upload

--- myjs1.js
+++ myjs1.js
@@ -1,7 +1,25 @@
-function myFunction() {
-var dots = document.getElementById("dots");
-var moreText = document.getElementById("more");
-var btnText = document.getElementById("myBtn");
+var text;
+function myFunction1() {
+var dots = document.getElementById("dots1");
+var moreText = document.getElementById("more1");
+var btnText = document.getElementById("myBtn1");
+
+if (dots.style.display === "none") {
+dots.style.display = "inline";
+btnText.innerHTML = "Read more";
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+btnText.innerHTML = "Read less";
+moreText.style.display = "inline";
+}
+}
+
+var text;
+function myFunction2() {
+var dots = document.getElementById("dots2");
+var moreText = document.getElementById("more2");
+var btnText = document.getElementById("myBtn2");
 
 if (dots.style.display === "none") {
 dots.style.display = "inline";
@@ -24,25 +42,38 @@ var signUpbtn = document.getElementById("signUpbtn");
 
 saveUser = function (formDetails){
   //set the user name and pwd into a properties file
+  //text.add
 }
-
-function showUserOld(){
-  //read username and pwd;
-  var myReader = new FileReader();
-  myReader.onload = function(e){
-    var text = myReader.result;
-  }
-  console.log("userDetails: " + myReader.readAsText("userDetails.json"));
-};
 
 function showUser() {
   //read username and pwd;
-    var text = '{"username" : "gavin","password" : "password"}';
+    text = '{"users":[{"username" : "gavin","password" : "password"}, {"username" : "atharva","password" : "password"}]}';
     //JSON.parse(this.responseText);
     var user = JSON.parse(text);
     console.log("userDetails: " + user + "username: " + user.username);
   }
 
+  function openPage(evt, pageName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+
+
+
+  function function3() {
+    alert("Thanks a lot for subscribing!");
+  }
 //$.getJSON("userDetails.json", function(result){
         //var text = "";//JSON.parse(userDetails);
         //$.getJSON('userDetails.json', function(data){
